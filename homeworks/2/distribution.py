fc50b5fa0ee6d0b34265f21c757a0f10898361f9
Ivan <74999728+IvanZuban@users.noreply.github.com>
2023-11-13 20:43:21 +0900

    Completed second HomeWork

---
+++ homeworks/2/distribution.py
@@ -0,0 +1,58 @@
+import numpy as np
+
+class LaplaceDistribution:    
+    @staticmethod
+    def mean_abs_deviation_from_median(x: np.ndarray):
+        '''
+        Args:
+        - x: A numpy array of shape (n_objects, n_features) containing the data
+          consisting of num_train samples each of dimension D.
+        '''
+        ####
+        # Do not change the class outside of this block
+        # Your code here
+        
+        median = np.median(x)
+        abs_deviations = np.abs(x - median)
+        mean_abs_deviation = np.mean(abs_deviations)
+
+        return mean_abs_deviation
+        
+        ####
+
+    def __init__(self, features):
+        '''
+        Args:
+            feature: A numpy array of shape (n_objects, n_features). Every column represents all available values for the selected feature.
+        '''
+        ####
+        # Do not change the class outside of this block
+        self.loc = np.median(features, axis=0) # YOUR CODE HERE 
+        self.scale = np.mean(np.abs(features - self.loc), axis=0) # YOUR CODE HERE
+        ####
+
+
+    def logpdf(self, values):
+        '''
+        Returns logarithm of probability density at every input value.
+        Args:
+            values: A numpy array of shape (n_objects, n_features). Every column represents all available values for the selected feature.
+        '''
+        ####
+        # Do not change the class outside of this block
+
+        abs_diff = np.abs(values - self.loc)
+        log_likelihood = -np.log(2 * self.scale) - abs_diff / self.scale
+        
+        return log_likelihood
+
+        ####
+        
+    
+    def pdf(self, values):
+        '''
+        Returns probability density at every input value.
+        Args:
+            values: A numpy array of shape (n_objects, n_features). Every column represents all available values for the selected feature.
+        '''
+        return np.exp(self.logpdf(values))
